8d7d908dd5fcdc9ea59af6cd01cfa598b1bf0d92
assafperetz6 <assafperetz6@gmail.com>
2024-11-30 14:32:15 +0200

    Add details and edit pages

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -3,10 +3,11 @@ import { store } from './store/store.js'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { AppFooter } from './cmps/AppFooter.jsx'
+
 import { HomePage } from './pages/HomePage.jsx'
 import { ToyIndex } from './pages/ToyIndex.jsx'
-
-
+import { ToyDetails } from './pages/ToyDetails.jsx'
+import { ToyEdit } from './pages/ToyEdit.jsx'
 
 function App() {
 
@@ -19,6 +20,8 @@ function App() {
                         <Routes>
                             <Route element={<HomePage />} path="/" />
                             <Route element={<ToyIndex />} path="/toy" />
+                            <Route element={<ToyDetails />} path="/toy/:toyId" />
+                            <Route element={<ToyEdit />} path="/toy/edit/:toyId" />
                         </Routes>
                     </main>
                     <AppFooter />
