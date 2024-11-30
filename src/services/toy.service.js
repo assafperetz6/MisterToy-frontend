91f0df2d2ad0d11480a2fd214b528a80af622961
assafperetz6 <assafperetz6@gmail.com>
2024-11-30 12:48:23 +0200

    Add frontend services

---
+++ src/services/toy.service.js
@@ -0,0 +1,55 @@
+import { httpService } from './http.service.js'
+import { utilService } from './util.service.js'
+
+const BASE_URL = 'toy/'
+
+export const toyService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyCar,
+    getDefaultFilter,
+    getRandomCar
+}
+
+function query(filterBy = {}) {
+    return httpService.get(BASE_URL, filterBy)
+}
+
+function getById(toyId) {
+    return httpService.get(BASE_URL + toyId)
+
+}
+
+function remove(toyId) {
+    return httpService.delete(BASE_URL + toyId)
+}
+
+function save(toy) {
+    if (toy._id) {
+        return httpService.put(BASE_URL + toy._id, toy)
+    } else {
+        return httpService.post(BASE_URL, toy)
+    }
+}
+
+function getEmptyCar() {
+    return {
+        vendor: '',
+        price: '',
+        speed: '',
+    }
+}
+
+function getRandomCar() {
+    return {
+        vendor: 'Susita-' + (Date.now() % 1000),
+        price: utilService.getRandomIntInclusive(1000, 9000),
+        speed: utilService.getRandomIntInclusive(90, 200),
+    }
+}
+
+function getDefaultFilter() {
+    return { txt: '', maxPrice: '', minSpeed: '' }
+}
---
+++ src/services/toy.service.local.js
@@ -0,0 +1,111 @@
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+import { userService } from './user.service.js'
+
+const STORAGE_KEY = 'toyDB'
+
+_createToys()
+
+export const toyService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyToy,
+    getRandomToy,
+    getDefaultFilter
+}
+
+function query(filterBy = {}) {
+    return storageService.query(STORAGE_KEY)
+        .then(toys => {
+            if (!filterBy.txt) filterBy.txt = ''
+            if (!filterBy.maxPrice) filterBy.maxPrice = Infinity
+            const regExp = new RegExp(filterBy.txt, 'i')
+            return toys.filter(toy =>
+            {
+                return regExp.test(toy.vendor) &&
+                toy.price <= filterBy.maxPrice
+
+            }
+            )
+        })
+}
+
+function getById(toyId) {
+    return storageService.get(STORAGE_KEY, toyId)
+}
+
+function remove(toyId) {
+    return storageService.remove(STORAGE_KEY, toyId)
+}
+
+function save(toy) {
+    if (toy._id) {
+        return storageService.put(STORAGE_KEY, toy)
+    } else {
+        // when switching to backend - remove the next line
+        toy.owner = userService.getLoggedinUser()
+        return storageService.post(STORAGE_KEY, toy)
+    }
+}
+
+function getEmptyToy() {
+    return {
+        name: '',
+        price: 0,
+        labels: [],
+        }
+}
+
+// function getRandomToy() {
+//     const labels = [
+//         'On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
+//         'Outdoor', 'Battery Powered'
+//     ]
+
+//     return {
+//         name: 'Talking Doll',
+//         price: 123,
+//         labels: ['Doll', 'Battery Powered', 'Baby'],
+//         createdAt: Date.now(),
+//         inStock: true,
+//         }
+// }
+
+function getDefaultFilter() {
+    return { txt: '', maxPrice: '' }
+}
+
+function _createToys() {
+    var toys = utilService.loadFromStorage(STORAGE_KEY)
+    if (toys && toys.length > 0) return
+
+    toys = [
+        {
+            _id: 't101',
+            name: 'Talking Doll',
+            price: 123,
+            labels: ['Doll', 'Battery Powered', 'Baby'],
+            createdAt: 1631031801011,
+            inStock: true,
+        },
+        {
+            _id: 't102',
+            name: 'Jigsaw Puzzle',
+            price: 80,
+            labels: ['Box game', 'Art', 'puzzle'],
+            createdAt: 16310313453411,
+            inStock: true,
+        },
+        {
+            _id: 't103',
+            name: 'Remote Controlled Car',
+            price: 250,
+            labels: ['On Wheels', 'Outdoor', 'Battery Powered'],
+            createdAt: 1631035672011,
+            inStock: true,
+        },
+    ]
+    utilService.saveToStorage(STORAGE_KEY, toys)
+}
