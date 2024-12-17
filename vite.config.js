65af42e1b9865d75c8b194fcdd69591e16a06aa3
assafperetz6 <assafperetz6@gmail.com>
2024-12-17 21:33:01 +0200

    Add build config to vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://127.0.0.1:5173',
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+	build: {
+		outDir: '../MisterToy-backend/public',
+		emptyOutDir: true
+	}
 })
